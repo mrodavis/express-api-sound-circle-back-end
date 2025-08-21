cebaee35aff2dac8ae0d03ada355875343aa9017
mrodavis <orville.davis@scrapit.io>
2025-08-21 15:39:42 -0400

    init commit

---
+++ controllers/test-jwt.js
@@ -0,0 +1,31 @@
+// controllers/test-jwt.js
+
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+
+
+router.get('/sign-token', (req, res) => {
+    const user = {
+    id: 1,
+    username: 'test',
+    password: 'test',
+  };
+    const token = jwt.sign({ user }, process.env.JWT_SECRET);
+
+    res.json({ token });
+});
+
+router.post('/verify-token', (req, res) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    // Add in verify method
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    res.json({ decoded });
+  } catch (err) {
+    res.status(401).json({ err: 'Invalid token.' });
+  }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,32 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const logger = require('morgan');
+const testJwtRouter = require('./controllers/test-jwt');
+const authRouter = require('./controllers/auth');
+const usersRouter = require('./controllers/users.js');
+const hootsRouter = require("./controllers/hoots.js");
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+const PORT = process.env.PORT ? process.env.PORT: 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(logger('dev'));
+app.use('/auth', authRouter);
+app.use('/test-jwt', testJwtRouter);
+app.use('/users', usersRouter);
+app.use("/hoots", hootsRouter);
+// Routes go here
+
+app.listen(PORT, () => {
+  console.log('The express app is ready!');
+});
