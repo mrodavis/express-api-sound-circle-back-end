f6a6f0da53df5d920655d956a23c278fed8ed295
mrodavis <orville.davis@scrapit.io>
2025-08-26 14:57:17 -0400

    cleaned up server.js and track.controller to be more uniformed with other controllers

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const testJwtRouter = require('./controllers/test-jwt');
 const authRouter = require('./controllers/auth');
 const usersRouter = require('./controllers/users.js');
 const soundBytesRouter = require("./controllers/soundBytes.js");
-const Tracks = require('./controllers/tracks.controller');
+const tracksRouter = require('./controllers/tracks.controller');
+const verifyToken = require('./middleware/verify-token');
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -26,12 +27,7 @@ app.use('/auth', authRouter);
 app.use('/test-jwt', testJwtRouter);
 app.use('/users', usersRouter);
 app.use("/sBytes", soundBytesRouter);
-// Routes go here
-app.post   ('/tracks',       Tracks.create);
-app.get    ('/tracks',       Tracks.index);
-app.get    ('/tracks/:id',   Tracks.show);
-app.put    ('/tracks/:id',   Tracks.update);
-app.delete ('/tracks/:id',   Tracks.destroy);
+app.use('/tracks', tracksRouter);
 
 // basic 404
 app.use((req, res) => res.status(404).json({ error: 'Not found' }));
